2790bc3c4851c113ccccf9c922e05f7356eb3319
Youssef Galal <youssefgalalnazem@gmail.com>
2022-09-04 09:00:21 +0200

    Add login and register routes and templates
    Routes are set up, and basic HTML files with navigation between them is configured.

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def home():
     return render_template("home.html")
 
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
