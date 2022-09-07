ede46b8c8df35a29054ee5380d4aec9878763136
Youssef Galal <youssefgalalnazem@gmail.com>
2022-09-07 10:50:38 +0200

    Finish authentication system
    Completed register, log in and log out functionalities. Removed validators from form. All validations are done server-side now. Errors are flashed by adding flash messages to layout.html.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect, url_for
+from time import sleep
+
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_required, login_user, logout_user
 from flask_login import current_user
@@ -29,19 +31,50 @@ class User(db.Model, UserMixin):
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-    submit = SubmitField("Register")
+    username = StringField(
+        # validators=[InputRequired(), Length(min=4, max=20)],
+        render_kw={
+            "placeholder": "Username",
+            "class": "form-control mt-3 mb-1"
+        })
+    password = PasswordField(
+        # validators=[InputRequired(), Length(min=8, max=20)],
+        render_kw={
+            "placeholder": "Password",
+            "class": "form-control mt-1 mb-1"
+        })
+    confirm_password = PasswordField(
+        # validators=[InputRequired(), Length(min=8, max=20)],
+        render_kw={
+            "placeholder": "Confirm password",
+            "class": "form-control mt-1 mb-3"
+        })
+    submit = SubmitField("Register", render_kw={
+        "class": "btn btn-dark mt-3 mb-3"
+    })
 
     def validate_username(self, username):
         if User.query.filter_by(username=username.data).first():
+            flash("Username already in use", "danger")
             raise ValidationError("Username already exists.")
 
 
 class LoginForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-    submit = SubmitField("Log in")
+    username = StringField(
+        # validators=[InputRequired(), Length(min=4, max=20)],
+        render_kw={
+            "placeholder": "Username",
+            "class": "form-control mt-3 mb-1"
+        })
+    password = PasswordField(
+        # validators=[InputRequired(), Length(min=8, max=20)],
+        render_kw={
+            "placeholder": "Password",
+            "class": "form-control mt-1 mb-3"
+        })
+    submit = SubmitField("Log in", render_kw={
+        "class": "btn btn-dark mt-3 mb-3"
+    })
 
 
 @app.route('/')
@@ -61,6 +94,7 @@ def dashboard():
 @app.route('/logout', methods=["GET", "POST"])
 def logout():
     logout_user()
+    flash("Logged out", "primary")
     return redirect(url_for("home"))
 
 
@@ -69,11 +103,26 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
+        if not form.username.data:
+            flash("Missing username", "danger")
+            return render_template("login.html", form=form)
+
+        if not form.password.data:
+            flash("Missing password", "danger")
+            return render_template("login.html", form=form)
+
         user = User.query.filter_by(username=form.username.data).first()
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
+                flash("Logged in successfully", "success")
                 return redirect(url_for("dashboard"))
+            else:
+                flash("Incorrect username or password", "danger")
+                return render_template("login.html", form=form)
+        else:
+            flash("Incorrect username or password", "danger")
+            return render_template("login.html", form=form)
     return render_template("login.html", form=form)
 
 
@@ -82,12 +131,29 @@ def register():
     form = RegisterForm()
 
     if form.validate_on_submit():
+        if not form.username.data:
+            flash("Missing username", "danger")
+            return render_template("register.html", form=form)
+
+        if not form.password.data:
+            flash("Missing password", "danger")
+            return render_template("register.html", form=form)
+
+        if not form.confirm_password.data:
+            flash("Missing password confirmation", "danger")
+            return render_template("register.html", form=form)
+
+        if form.password.data != form.confirm_password.data:
+            flash("Confirmed password doesn't match password", "danger")
+            return render_template("register.html", form=form)
+
         hashed_password = bcrypt.generate_password_hash(form.password.data)
         new_user = User()
         new_user.username = form.username.data
         new_user.password = hashed_password
         db.session.add(new_user)
         db.session.commit()
+        flash('Registered successfully', "success")
         return redirect(url_for("login"))
 
     return render_template("register.html", form=form)
